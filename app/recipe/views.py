637e5adff5fd99392db2180cff137809e5866341
cyberpunk-akash <hegde.akash1999@gmail.com>
2020-03-01 22:38:03 +0530

    Added recipe image upload endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets, mixins, generics
+from rest_framework import viewsets, mixins, generics, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class BaseAttrViewSet(viewsets.GenericViewSet,
                      mixins.ListModelMixin,
@@ -46,12 +49,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
 
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
+
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create new recipe"""
         serializer.save(user=self.request.user)
 
+    @action(methods=['POST'],detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload image to recipe"""
+        recipe=self.get_object()
+        serializer=self.get_serializer(
+        recipe,
+        data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_200_OK)
+
+        return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+        )
     # def get_object(self):
     #     """Retrieve and return authenticated user"""
     #     return self.request.
