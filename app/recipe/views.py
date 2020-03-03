f90b775c10d4a4bf9947a426db0545de71f49f53
cyberpunk-akash <hegde.akash1999@gmail.com>
2020-03-03 11:18:36 +0530

    Feature to filter recipes, tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,7 +17,15 @@ class BaseAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for authenticated users only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+        int(self.request.query_params.get('assigned_only',0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new object"""
@@ -40,9 +48,34 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def _params_to_ints(self,qs):
+        """Convert list of string ids to list of int ids"""
+        return [int(str_id) for str_id in qs.split(',')]
+
+
     def get_queryset(self):
         """Return recipes for authenticated users only"""
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        # print('-----------------------------------------------------')
+        # print(tags)
+        # print(ingredients)
+        #
+        # print('-----------------------------------------------------')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            # print(ingredient_ids)
+            # print('-----------------------------------------------------')
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+            # print(queryset)
+            # print('-----------------------------------------------------')
+
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
@@ -112,7 +145,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
 #     permission_classes = (IsAuthenticated,)
 #     queryset = Ingredient.objects.all()
 #     serializer_class = serializers.IngredientSerializer
-#
+
+
 #     def get_queryset(self):
 #         """Returns objects for the current authenticated user only"""
 #         return self.queryset.filter(user=self.request.user).order_by('-name')
